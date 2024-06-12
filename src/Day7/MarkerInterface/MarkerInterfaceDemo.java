ea7e9125b07d0b95d7e8d5e5520888e4b889cbf4
Mustafa Khan GZ <mustafakhangz111@gmail.com>
2024-06-12 22:59:31 +0530

    Day7 commit

--- src/Day7/MarkerInterface/MarkerInterfaceDemo.java
+++ src/Day7/MarkerInterface/MarkerInterfaceDemo.java
@@ -5,7 +5,7 @@ public class MarkerInterfaceDemo {
 
 	public static void main(String[] args) {
 		Student s=new Student(101,"Neha",12000,"C++");
-		System.out.println(s);
+		
 		if (s instanceof Registrable )
 			System.out.println("Student is registered for the course");
 		else
