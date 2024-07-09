1b2dae20fb250f0e27710acd3cbf9db21e9ceb75
Mustafa Khan GZ <mustafakhangz111@gmail.com>
2024-07-09 18:27:08 +0530

    Day19 commit

--- src/Day19/FirstTestDemo.java
+++ src/Day19/FirstTestDemo.java
@@ -1,7 +1,7 @@
 //Program to demonstrate first test case
 package Day19;
 
-import org.junit.Test;
+import org.junit.Test; // download the JUnit and activate then we have to import this for testing purposes
 
 public class FirstTestDemo {
 	@Test
