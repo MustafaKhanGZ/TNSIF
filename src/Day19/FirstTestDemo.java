2f319a905f6bce7ae2f7c521c0ebc35d2a700f78
Mustafa Khan GZ <mustafakhangz111@gmail.com>
2024-07-09 18:13:31 +0530

    Day19 commit

---
+++ src/Day19/FirstTestDemo.java
@@ -0,0 +1,11 @@
+//Program to demonstrate first test case
+package Day19;
+
+import org.junit.Test;
+
+public class FirstTestDemo {
+	@Test
+	 public void firstTest() {
+		System.out.println("Hello");
+	}
+}
